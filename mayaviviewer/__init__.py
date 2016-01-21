e1896260cb2448b3190243bddc17df273657b95f
juzhang <jzha263@aucklanduni.ac.nz>
2016-01-21 22:03:53 +1300

    full import paths for mappluginutils.mayaviviewer

--- mayaviviewer/__init__.py
+++ mayaviviewer/__init__.py
@@ -8,20 +8,20 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 ===============================================================================
 """
 
-import os, sys
+# import os, sys
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-if current_dir not in sys.path:
-    # Using __file__ will not work if py2exe is used,
-    # Possible problem of OSX10.6 also.
-    sys.path.insert(0, current_dir)
+# current_dir = os.path.dirname(os.path.abspath(__file__))
+# if current_dir not in sys.path:
+#     # Using __file__ will not work if py2exe is used,
+#     # Possible problem of OSX10.6 also.
+#     sys.path.insert(0, current_dir)
 
 # import class that derives itself from the step mountpoint.
-from mayaviscenewidget import MayaviSceneWidget
-from mayaviviewerobjects import colours, MayaviViewerObjectsContainer, MayaviViewerSceneObject, MayaviViewerObject
-from mayaviviewerfieldworkmeasurements import MayaviViewerFemurMeasurements
-from mayaviviewerfieldworkmodel import MayaviViewerFieldworkModel
-from mayaviviewergiasscan import MayaviViewerGiasScan
-from mayaviviewerdatapoints import MayaviViewerDataPoints
-from mayaviviewerlandmark import MayaviViewerLandmark
-from mayaviviewerimageplane import MayaviViewerImagePlane
+from gias2.mappluginutils.mayaviviewer.mayaviscenewidget import MayaviSceneWidget
+from gias2.mappluginutils.mayaviviewer.mayaviviewerobjects import colours, MayaviViewerObjectsContainer, MayaviViewerSceneObject, MayaviViewerObject
+from gias2.mappluginutils.mayaviviewer.mayaviviewerfieldworkmeasurements import MayaviViewerFemurMeasurements
+from gias2.mappluginutils.mayaviviewer.mayaviviewerfieldworkmodel import MayaviViewerFieldworkModel
+from gias2.mappluginutils.mayaviviewer.mayaviviewergiasscan import MayaviViewerGiasScan
+from gias2.mappluginutils.mayaviviewer.mayaviviewerdatapoints import MayaviViewerDataPoints
+from gias2.mappluginutils.mayaviviewer.mayaviviewerlandmark import MayaviViewerLandmark
+from gias2.mappluginutils.mayaviviewer.mayaviviewerimageplane import MayaviViewerImagePlane
